269f8c57e03891eadf2ef65431298acd038ad779
Hiroshi Matsuyama <Hiroshi.Matsuyama@sony.com>
2024-10-23 12:54:04 +0900

    Fixed CMakeLists.txt to use oscpack from our project.

--- generate_vs_solution_2022.bat
+++ generate_vs_solution_2022.bat
@@ -1,5 +1,7 @@
 @echo OFF
 
+set CMAKE_PATH="C:\Program Files\Microsoft Visual Studio\2022\Professional\Common7\IDE\CommonExtensions\Microsoft\CMake\CMake\bin\cmake.exe"
+
 if exist build/ (
   rd /s /q build
 )
@@ -10,7 +12,13 @@ if exist install/ (
 )
 
 pushd build
-"C:\Program Files\Microsoft Visual Studio\2022\Professional\Common7\IDE\CommonExtensions\Microsoft\CMake\CMake\bin\cmake.exe" -G "Visual Studio 17 2022" ..
+%CMAKE_PATH%  -G "Visual Studio 17 2022" ..
 popd
+
+echo ===== Creating visual studio solution completed. =====
+
+echo ===== Start building third party libraries. =====
+call build_third_party_vs2022.bat
+echo ===== End building third party libraries. =====
 
 PAUSE
